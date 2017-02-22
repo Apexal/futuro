f37605fabf21b9260f4a70b0b68f358374457243
Frank Matranga <thefrankmatranga@gmail.com>
2017-02-22 10:53:08 -0500

    Added time placeholders to form

--- server/modules/schemas.js
+++ server/modules/schemas.js
@@ -5,6 +5,8 @@ const Schema = mongoose.Schema;
 module.exports = {
     Activity: {
         date: { type: Date, required: true },
+        startTime: String,
+        endTime: String, /* '9:10 AM' (optional) */
         summary: String,
         description: String,
         value: { type: Number, default: 0 }
