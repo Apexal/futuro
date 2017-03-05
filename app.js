9994c42ad3b78c4b43f7fbf1f54fabbde55431b1
Frank Matranga <thefrankmatranga@gmail.com>
2017-03-05 02:19:37 -0500

    Improved folder system

--- app.js
+++ app.js
@@ -17,9 +17,9 @@ const mongodb = require('./server/modules/mongodb.js');
 const app = express();
 
 // View engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'server'));
 app.set('view engine', 'pug');
-app.locals.basedir = path.join(__dirname, 'views');
+app.locals.basedir = path.join(__dirname, 'server');
 
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico'))); TODO: add favicon
 app.use(compression());
